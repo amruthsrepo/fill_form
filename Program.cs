5760b3eb6f900b53c097e69e7880ec7ccca3a38b
Amruth Nag <51161103+amruthsrepo@users.noreply.github.com>
2024-02-03 14:09:19 -0500

    Open url

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
+namespace fill_form
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+        }
+    }
+}
