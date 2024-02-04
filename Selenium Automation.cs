890da6a16940c5b561f1a53cbe513c8cba5225af
Amruth Nag <51161103+amruthsrepo@users.noreply.github.com>
2024-02-04 14:21:39 -0500

    search for jobs, open a result and fill person details

---
+++ Selenium Automation.cs
@@ -0,0 +1,30 @@
+﻿
+namespace fill_form
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, string> person = new()
+            {
+                { "firstname", "John" },
+                { "lastname", "Doe" },
+                { "phone", "999-999-9999" },
+                { "email", "test@gmail.com" },
+                { "city", "testCity" }
+            };
+            string[] searchStrings = { "\"software\" developer engineer site:greenhouse.io \n" };
+
+            WebOps web = new(person);
+
+            /// Load google and search using the searchStrings array.
+            web.LoadGoogle(searchStrings);
+            /// Open the first result and apply for the job.
+            web.OpenResultsAndApply();
+
+            /// This function is for testing purposes only.
+            // web._OpenJobAndApply("https://asana.com/jobs/apply/5607560?gh_jid=5607560");
+        }
+    }
+}
--- WebOps.cs
+++ WebOps.cs
@@ -4,6 +4,7 @@ using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 using IWebElementArray = System.Collections.ObjectModel.ReadOnlyCollection<OpenQA.Selenium.IWebElement>;
+using StringBuilder = System.Text.StringBuilder;
 
 namespace fill_form
 {
@@ -17,12 +18,17 @@ namespace fill_form
         Actions actions;
         readonly string ctrlKey;
         string mainTab = "";
+        string[] details;
+        Dictionary<string, string> personDetails;
 
         /// <summary>
         /// Constructor for WebOps class. Initializes WebDriver, WebDriverWait and Actions.
         /// </summary>
-        public WebOps()
+        /// <param name="personDetails">The details of the person applying for the job.</param>
+        public WebOps(Dictionary<string, string> personDetails)
         {
+            this.personDetails = personDetails;
+            details = personDetails.Keys.ToArray();
             driver = new ChromeDriver();
             driverWait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
             actions = new Actions(driver);
@@ -35,7 +41,7 @@ namespace fill_form
         /// </summary>
         /// <param name="searchBy">The criteria to search the element by.</param>
         /// <returns>The found element as an IWebElement.</returns>
-        public IWebElement WaitAndGetElement(By searchBy)
+        IWebElement WaitAndGetElement(By searchBy)
         {
             IWebElement? element = default;
             while (element == null)
@@ -58,7 +64,7 @@ namespace fill_form
         /// </summary>
         /// <param name="searchBy">The criteria to search the elements by.</param>
         /// <returns>The found elements as a System.Collections.ObjectModel.ReadOnlyCollection of OpenQA.Selenium.IWebElement.</returns>
-        public IWebElementArray WaitAndGetElements(By searchBy)
+        IWebElementArray WaitAndGetElements(By searchBy)
         {
             IWebElementArray? elements = null;
             while (elements == null)
@@ -82,7 +88,7 @@ namespace fill_form
         /// </summary>
         /// <param name="link">The link to open.</param>
         /// <returns>The handle of the new tab as a string.</returns>
-        public string OpenLinkInNewTab(IWebElement link)
+        string OpenLinkInNewTab(IWebElement link)
         {
             actions.KeyDown(ctrlKey)
                     .Click(link)
@@ -93,22 +99,56 @@ namespace fill_form
         }
 
         /// <summary>
+        /// Returns a string with only the characters from a-z or A-Z.
+        /// </summary>
+        /// <param name="str">The string to return only characters from.</param>
+        /// <returns>The string with only characters from a-z or A-Z.</returns>
+        static string ReturnOnlyChars(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in str)
+            {
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
         /// Loads Google and performs a search.
         /// </summary>
         /// <param name="searchString">The string to search for.</param>
-        public void LoadGoogle(string searchString)
+        public void LoadGoogle(string[] searchStrings)
         {
             string webUrl = "https://www.google.com/";
+
+            foreach (string searchString in searchStrings)
+            {
+                driver.Navigate().GoToUrl(webUrl);
+                mainTab = driver.WindowHandles.ElementAt(0);
+                IWebElement searchBox = WaitAndGetElement(By.TagName("textArea"));
+                searchBox.SendKeys(searchString);
+            }
+        }
+
+        /// <summary>
+        /// !!! Warning: This method is for testing purposes only. !!!
+        /// Opens a job and applies for it.
+        /// </summary>
+        /// <param name="webUrl">The URL of the job to open and apply for.</param>
+        public void _OpenJobAndApply(string webUrl)
+        {
             driver.Navigate().GoToUrl(webUrl);
             mainTab = driver.WindowHandles.ElementAt(0);
-            IWebElement searchBox = WaitAndGetElement(By.TagName("textArea"));
-            searchBox.SendKeys(searchString);
+            ApplyForJob();
         }
 
         /// <summary>
         /// Opens the search results and applies for the job.
         /// </summary>
-        public void OpenResultsAndApply(Dictionary<string, string> personDetails)
+        public void OpenResultsAndApply()
         {
             IWebElementArray links = WaitAndGetElements(By.TagName("h3"));
 
@@ -124,22 +164,32 @@ namespace fill_form
         /// <summary>
         /// Applies for a job by filling out the application form.
         /// </summary>
-        public void ApplyForJob()
+        void ApplyForJob()
         {
             IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)driver;
             javaScriptExecutor.ExecuteScript("window.scrollBy(0,document.body.scrollHeight)");
 
             IWebElement application = WaitAndGetElement(By.CssSelector("iframe[title='Greenhouse Job Board']"));
+            actions.MoveToElement(application).Perform();
             driver.SwitchTo().Frame(application);
 
             IWebElementArray textBoxes = WaitAndGetElements(By.TagName("input"));
 
             foreach (IWebElement textBox in textBoxes)
             {
-                Console.WriteLine(textBox.TagName);
-                Console.WriteLine(textBox.GetAttribute("id"));
+                string field = ReturnOnlyChars(textBox.GetAttribute("id"));
+                Console.WriteLine($"field: {field}");
+                if (field.Length < 1) continue;
+
+                for (int i = 0; i < details.Length; i++)
+                {
+                    if (details[i].Contains(field))
+                    {
+                        textBox.SendKeys(personDetails[details[i]]);
+                        break;
+                    }
+                }
             }
         }
-
     }
 }
