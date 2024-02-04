cba858fea15b06e694d4935f16547ad8b0ef9fa0
rahulgovindkumar <rahulgovindkumar@gmail.com>
2024-02-04 12:31:49 -0600

    Intial read data from Excel using EPPlus

---
+++ ExcelDataModal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+
+
+namespace fill_form
+{
+    class ExcelDataModal
+    {
+
+        string path = "";
+     
+
+        public ExcelDataModal(string path, int sheetIndex)
+        {
+            this.path = path;
+  
+        }
+
+        public Dictionary<string, int> GetSheetNamesWithIndex()
+        {
+            Dictionary<string, int> sheetNamesWithIndex = new Dictionary<string, int>();
+
+
+            // Set the LicenseContext property to comply with EPPlus licensing
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            
+
+            using (var package = new ExcelPackage(new FileInfo(path)))
+            {
+
+                Console.WriteLine(package.Workbook.Worksheets.Count);
+                for (int i = 0; i < package.Workbook.Worksheets.Count; i++)
+                {
+                    // Access the worksheet by position
+                    var worksheet = package.Workbook.Worksheets[i];
+                    sheetNamesWithIndex.Add(worksheet.Name, i);
+                }
+            }
+
+            return sheetNamesWithIndex;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Reflection;
+
 
 namespace fill_form
 {
@@ -7,8 +9,31 @@ namespace fill_form
     {
         static void Main(string[] args)
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://www.google.com/");
+ 
+            var relativePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string path = (relativePath + @"\Data\Info\MasterList.xlsx");
+            Console.WriteLine(path);
+            
+            //Create an instance of the ExcelDataModal class
+            ExcelDataModal excelDataModal = new ExcelDataModal(path, 1);
+
+            //Get All the sheetname with indices
+            Dictionary<string, int> sheetNamesWithIndex = excelDataModal.GetSheetNamesWithIndex();
+
+            // Display sheet names with indices
+            Console.WriteLine("Sheet Names and Indices:");
+            foreach (var kvp in sheetNamesWithIndex)
+            {
+                Console.WriteLine($"{kvp.Key} - Index: {kvp.Value}");
+            }
+
+
+
+            //exceldatamodal exceldatamodal = new exceldatamodal(path, sheetindex);
+            //dictionary<string, string> data = exceldatamodal.readdata();
+
+
+
         }
     }
 }
